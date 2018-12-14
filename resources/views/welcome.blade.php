11600af7f628bc5552f0869d17ecced4fece6a12
Yujuyeon <remake382@gmail.com>
2018-12-14 14:11:23 +0900

    lara

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Live Quiz Show
+                    Live Quiz Show123
                 </div>
 
                 <div class="links">
